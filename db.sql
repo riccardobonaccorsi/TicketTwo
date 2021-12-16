8fffcc0655fd4b1ea73e79b597d6efa776bca594
riccardobonaccorsi <74291950+riccardobonaccorsi@users.noreply.github.com>
2021-12-16 10:35:53 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,27 +1,26 @@
--- MariaDB dump 10.19  Distrib 10.5.12-MariaDB, for debian-linux-gnueabihf (armv7l)
---
--- Host: localhost    Database: tickettwo
--- ------------------------------------------------------
--- Server version	10.5.12-MariaDB-1build1
+DROP DATABASE IF EXISTS tickettwo;
+CREATE DATABASE ticckettwo;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `cliente`
---
+DROP TABLE IF EXISTS `utente`;
+CREATE TABLE `utente` (
+  `UID` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(25) NOT NULL,
+  `nome` varchar(25) NOT NULL,
+  `psw` varchar(25) NOT NULL,
+  PRIMARY KEY (`UID`)
+);
+INSERT INTO `utente` VALUES ('Ricky', 'rickyb@gmail.com', 'Speck&Grana'),
+	('Davide', 'davimb@gmail.com', 'magnagatti01'),
+ 	('Elena', 'ele@outlook.it', 'crauti.wurstel'),
+	('MegaEventi', 'money@gmail.com', 'EventoUgualeSoldi'),
+	('Giorgio', 'giogioacciuga@outlook.it', 'cannolino89'),
+	('Sofia', 'sofi.auro@gmail.com', 'cioccolatonutella'),
+	('Federica', 'fedelove@gmail.com', 'LoveLoveLove'),
+	('Anna', 'annina@outlook.it', 'BelloIlSole'),
+	('BestGestore', 'the.best@gmail.com', 'SoloIlTop'),
+	('Ginevra', 'gine@outlook.it', 'risottino54');
 
 DROP TABLE IF EXISTS `cliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cliente` (
   `UID` int(11) NOT NULL,
   `data_nascita` date NOT NULL,
@@ -29,25 +28,28 @@ CREATE TABLE `cliente` (
   `residenza` varchar(25) NOT NULL,
   PRIMARY KEY (`UID`),
   CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `utente` (`UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
+INSERT INTO `cliente` VALUES ( 1, 'Bonaccorsi', "2000-06-10", 'Google Pay', 'Trento' ),
+	( 2, 'Morassut', "1994-12-20", 'Google Pay', 'Vicenza' ),
+	( 3, 'Badole', "2001-12-03", 'Carta di credito', 'Belluno' ),
+	( 5, 'Datteron', "1970-10-25", 'Apple Pay', 'Caltanissetta' ),
+	( 6, 'Zatta', "1999-01-01", 'Google Pay', 'Bologna' ),
+	( 7, 'Dalle Sasse', "2000-09-15", 'Carta di credito', 'Roma' ),
+	( 8, 'Dal Pra', "1989-11-30", 'Google Pay', 'Firenze' ),
+	( 10, 'Ventoso', "2001-09-28", 'Postepay', 'Trento' );
+  
 
---
--- Dumping data for table `cliente`
---
-
-LOCK TABLES `cliente` WRITE;
-/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `evento`
---
+DROP TABLE IF EXISTS `gestore`;
+CREATE TABLE `gestore` (
+  `UID` int(11) NOT NULL,
+  `dati_bancari` varchar(25) NOT NULL,
+  PRIMARY KEY (`UID`),
+  CONSTRAINT `gestore_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `utente` (`UID`)
+);
+INSERT INTO `gestore` VALUES (4, 'Unicredit S.p.a'),
+	(9, 'Poste Italiane - Postepay');
 
 DROP TABLE IF EXISTS `evento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `evento` (
   `EID` int(11) NOT NULL AUTO_INCREMENT,
   `UID` int(11) NOT NULL,
@@ -61,77 +63,10 @@ CREATE TABLE `evento` (
   PRIMARY KEY (`EID`),
   KEY `UID` (`UID`),
   CONSTRAINT `evento_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `gestore` (`UID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
+INSERT INTO `evento` VALUES (9, 'Ferrock Festival', "2022-07-10 21:00:00", "2022-07-16 01:00:00", 'Vicenza','Vari',	'Rock', '2,00'),
+	(9, 'Eurovision', '20221110 20:00:00', "2022-11-16 02:00:00", 'Torino', 'Maneskin', 'Vari', '168,00'),
+	(4, 'MagicTrap', "2021-12-31 23:00:00", "2022-01-01 06:00:00" 'Milano', 'Sfera', 'Trap', '200,00'),
+	(9, 'HotMusic', "2022-06-09 24:00:00", "2022-06-10 05:00:00", 'Roma',	'Vari', 'Dance', '42,00'),
+	(4, 'Vasco a Trento', "2021-12-17 19:00:00", "2021-1219 23:00:00", 'Trento',	'Vasco Rossi', 'Rock', '58,00');
 
---
--- Dumping data for table `evento`
---
-
-LOCK TABLES `evento` WRITE;
-/*!40000 ALTER TABLE `evento` DISABLE KEYS */;
-INSERT INTO `evento` VALUES (1,2,'prova','2022-01-15 16:30:00','2022-01-15 19:30:00','mesiano','Radio 105','misto',0);
-/*!40000 ALTER TABLE `evento` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `gestore`
---
-
-DROP TABLE IF EXISTS `gestore`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gestore` (
-  `UID` int(11) NOT NULL,
-  `dati_bancari` varchar(25) NOT NULL,
-  PRIMARY KEY (`UID`),
-  CONSTRAINT `gestore_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `utente` (`UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gestore`
---
-
-LOCK TABLES `gestore` WRITE;
-/*!40000 ALTER TABLE `gestore` DISABLE KEYS */;
-INSERT INTO `gestore` VALUES (2,'datibanca');
-/*!40000 ALTER TABLE `gestore` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `utente`
---
-
-DROP TABLE IF EXISTS `utente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `utente` (
-  `UID` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(25) NOT NULL,
-  `nome` varchar(25) NOT NULL,
-  `psw` varchar(25) NOT NULL,
-  PRIMARY KEY (`UID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `utente`
---
-
-LOCK TABLES `utente` WRITE;
-/*!40000 ALTER TABLE `utente` DISABLE KEYS */;
-INSERT INTO `utente` VALUES (1,'prova@prova','prova','prova'),(2,'prova@unitn','unitn','unitn');
-/*!40000 ALTER TABLE `utente` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-12-02  8:28:53
