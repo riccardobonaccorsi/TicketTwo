ebadc234be4ea05bca183c4f8292558f4a0e0371
riccardobonaccorsi <riccardo.bonaccorsi@studenti.unitn.it>
2021-12-16 16:22:28 +0100

    Creazione pagina dati utente, mora

--- sito.js
+++ sito.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var session = require('express-session');
+const session = require('express-session');
 const mysql = require('mysql');
 const bodypars = require('body-parser');
 const Connection = require('mysql/lib/Connection');
@@ -41,7 +41,8 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
     var ssn = req.session;
     change_file('login_err.html');
-    var q = `SELECT UID, nome, cliente, gestore FROM utente WHERE email="${req.body.email}" AND psw = "${req.body.psw}";`;
+    var q = `SELECT UID, nome, is_gestore FROM utente WHERE email="${req.body.email}" AND psw = "${req.body.psw}";`;
+    console.log(q);
     db.query(q, (err, result) => {
         if (err) {
             res.sendFile(file_upload);
@@ -51,8 +52,7 @@ app.post("/", (req, res) => {
             change_file('lista.html');
             ssn.ID = row.UID;
             ssn.nome = row.nome;
-            ssn.cliente = row.cliente;
-            ssn.gestore = row.gestore;
+            ssn.is_gestore = row.is_gestore;
         });
         if (file_upload == __dirname + '/public/SITO/lista.html') {
             res.redirect(`/eventi`);
@@ -70,54 +70,109 @@ app.get("/login_err", (req, res) => {
 
 app.get('/eventi', (req, res) => {
     var ssn = req.session;
-    console.log(ssn.cliente);
-    if (ssn.cliente == 1) {
-        change_file('lista.html');
-        console.log("cliente");
-        q = 'SELECT * FROM evento';
-        console.log(q);
-        db.query(q, (err, result) => {
-            if (err) throw err;
-            var fs = require('fs');
-            fs.readFile(file_upload, 'utf8', (err, data)=> {
-                if (err) throw err;
-                var $ = require('cheerio').load(data);
-                var out = "<table>"
-                out = out + "<tr><th>EID</th><th>UID</th><th>Nome</th><th>data inizio</th><th>data fine</th><th>luogo</th><th>artisti</th><th>genere</th><th>prezzo</th><th></th></tr>";
-                result.forEach( (row) => {
-                    var data_i = row.d_inizio;
-                    var data_f = row.d_fine;
-                    out = out + `<tr><td>${row.EID}</td><td><a href='/API/ricercaGestore?UID=${row.UID}'>${row.UID}</a></td><td>${row.nome}</td><td>${data_i}</td><td>${data_f}</td><td>${row.luogo}</td><td>${row.artisti}</td><td>${row.genere}</td><td>${row.prezzo}</td><td><button>m</button><button>e</button></td></tr>`
-                });
-                out = out + "</table>";
-                
-                res.send($.html().replace('<p>/lista/</p>', out));
-            });
-        });
-    } else if (ssn.cliente == 0) {
-        console.log("gesote");
-        q = `SELECT * FROM evento WHERE UID = ${ssn.ID}`;
-        console.log(q);
-        db.query(q, (err, result) => {
-            if (err) throw err;
-            var fs = require('fs');
-            fs.readFile(file_upload, 'utf8', (err, data)=> {
-                if (err) throw err;
-                var $ = require('cheerio').load(data);
-                var out = "<table>"
-                out = out + "<tr><th>EID</th><th>UID</th><th>Nome</th><th>data inizio</th><th>data fine</th><th>luogo</th><th>artisti</th><th>genere</th><th>prezzo</th><th></th></tr>";
-                risultati.forEach( (row) => {
-                    var data_i = row.d_inizio;
-                    var data_f = row.d_fine;
-                    out = out + `<tr><td>${row.EID}</td><td><a href='/API/ricercaGestore?UID=${row.UID}'>${row.UID}</a></td><td>${row.nome}</td><td>${data_i}</td><td>${data_f}</td><td>${row.luogo}</td><td>${row.artisti}</td><td>${row.genere}</td><td>${row.prezzo}</td><td><button>m</button><button>e</button></td></tr>`
-                });
-                out = out + "</table>";
-                res.send($.html().replace('<p>/lol/</p>', out));
-            });
-        });
-    } else {
+    console.log(ssn);
+    console.log(ssn.is_gestore);
+    if (ssn.is_gestore === undefined) {
         res.redirect("/");
+    } else {
+        if (ssn.is_gestore == 0) {
+            change_file('lista.html');
+            console.log("cliente");
+            q = 'SELECT * FROM evento';
+            console.log(q);
+            db.query(q, (err, result) => {
+                if (err) throw err;
+                var fs = require('fs');
+                fs.readFile(file_upload, 'utf8', (err, data)=> {
+                    if (err) throw err;
+                    var $ = require('cheerio').load(data);
+                    var out = "<table>"
+                    out = out + "<tr><th>EID</th><th>UID</th><th>Nome</th><th>data inizio</th><th>data fine</th><th>luogo</th><th>artisti</th><th>genere</th><th>prezzo</th><th></th></tr>";
+                    result.forEach( (row) => {
+                        var data_i = row.d_inizio;
+                        var data_f = row.d_fine;
+                        out = out + `<tr><td>${row.EID}</td><td><a href='/API/ricercaGestore?UID=${row.UID}'>${row.UID}</a></td><td>${row.nome}</td><td>${data_i}</td><td>${data_f}</td><td>${row.luogo}</td><td>${row.artisti}</td><td>${row.genere}</td><td>${row.prezzo}</td><td><button>m</button><button>e</button></td></tr>`
+                    });
+                    out = out + "</table>";
+                    
+                    res.send($.html().replace('<p>/lista/</p>', out));
+                });
+            });
+        } else if (ssn.is_gestore == 1) {
+            console.log("gesote");
+            q = `SELECT * FROM evento WHERE UID = ${ssn.ID}`;
+            console.log(q);
+            db.query(q, (err, result) => {
+                if (err) throw err;
+                var fs = require('fs');
+                fs.readFile(file_upload, 'utf8', (err, data)=> {
+                    if (err) throw err;
+                    var $ = require('cheerio').load(data);
+                    var out = "<table>"
+                    out = out + "<tr><th>EID</th><th>UID</th><th>Nome</th><th>data inizio</th><th>data fine</th><th>luogo</th><th>artisti</th><th>genere</th><th>prezzo</th><th></th></tr>";
+                    risultati.forEach( (row) => {
+                        var data_i = row.data_inizio;
+                        var data_f = row.data_fine;
+                        out = out + `<tr><td>${row.EID}</td><td><a href='/API/ricercaGestore?UID=${row.UID}'>${row.UID}</a></td><td>${row.nome}</td><td>${data_i}</td><td>${data_f}</td><td>${row.luogo}</td><td>${row.artisti}</td><td>${row.genere}</td><td>${row.prezzo}</td><td><button>m</button><button>e</button></td></tr>`
+                    });
+                    out = out + "</table>";
+                    res.send($.html().replace('<p>/lol/</p>', out));
+                });
+            });
+        }
     }
 });
 
-// 
+// -----------------------------------------
+
+app.get('/info_utente', (req, res) => {
+    var ssn = req.session;
+    // console.log(ssn.);
+    if (ssn.is_gestore === undefined) {
+        res.redirect("/");
+    } else {
+        if (ssn.is_gestore == 0) {
+            change_file('dati_utente.html');
+            console.log("cliente");
+            q =`SELECT cliente.UID, cognome, nome, data_nascita, metodo_pagamento, residenza FROM cliente, utente WHERE utente.UID = cliente.UID AND utente.UID = ${ssn.ID} ;`;
+            console.log(q);
+            db.query(q, (err, result) => {
+                if (err) throw err;
+                var fs = require('fs');
+                fs.readFile(file_upload, 'utf8', (err, data)=> {
+                    if (err) throw err;
+                    var $ = require('cheerio').load(data);
+                    var out = "<table>"
+                    out = out + "<tr><th>UID</th><th>Nome</th><th>Cognome</th><th>data_nascita</th><th>metodo_pagamento</th><th>residenza</th></tr>";
+                    var dati = result[0];
+                    var birth = dati.data_nascita.getDate() + '/';
+                    birth = birth + (dati.data_nascita.getMonth() + 1) + '/';
+                    birth = birth + dati.data_nascita.getFullYear();
+                    out = out + `<tr><td>${dati.UID}</td><td>${dati.nome}</td><td>${dati.cognome}</td><td>${birth}</td><td>${dati.metodo_pagamento}</td><td>${dati.residenza}</td></tr>`;
+                    out = out + "</table>";
+                    
+                    res.send($.html().replace('<p>/tabella/</p>', out));
+                });
+            });
+        } else if (ssn.is_gestore == 1) {
+            console.log("gestore");
+            q = `SELECT gestore.UID, nome, dati_bancari FROM gestore, utente WHERE utente.UID = gestore.UID AND UID = ${ssn.ID}`;
+            console.log(q);
+            db.query(q, (err, result) => {
+                if (err) throw err;
+                var fs = require('fs');
+                fs.readFile(file_upload, 'utf8', (err, data)=> {
+                    if (err) throw err;
+                    var $ = require('cheerio').load(data);
+                    var out = "<table>"
+                    out = out + "<tr><th>UID</th><th>Nome</th><th>dati_bancari</th></tr>";
+                    var dati = result[0];
+                    out = out + `<tr><td>${dati.UID}</td><td>${dati.nome}</td><<td>${dati.dati_bancari}</td></tr>`;
+                    out = out + "</table>";
+                    
+                    res.send($.html().replace('<p>/tabella/</p>', out));
+                });
+            });
+        }
+    }
+});
